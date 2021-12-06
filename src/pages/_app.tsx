6e13b79962d75a4873de8495bb1ba9853ae8752c
cnrad <conradwcrawford@gmail.com>
2021-12-05 22:29:18 -0500

    meta: move head to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,32 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SWRConfig } from "swr";
+import Head from "next/head";
+import { information } from "../info";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <SWRConfig
-            value={{
-                refreshInterval: 3000,
-                fetcher: async (url: string) => await fetch(url).then(res => res.json()),
-            }}
-        >
-            <Component {...pageProps} />
-        </SWRConfig>
+        <>
+            <Head>
+                <title>Sponsor @{information.username} on GitHub</title>
+                <link rel="icon" type="image/png" href="/favicon.png" />
+                <meta
+                    name="description"
+                    content={`Sponsor ${information.username} on an easier alternative to GitHub Sponsors.`}
+                />
+                <meta name="theme-color" content="#ffffff" />
+                <meta name="image" content="/favicon.png" />
+            </Head>
+
+            <SWRConfig
+                value={{
+                    refreshInterval: 3000,
+                    fetcher: async (url: string) => await fetch(url).then(res => res.json()),
+                }}
+            >
+                <Component {...pageProps} />
+            </SWRConfig>
+        </>
     );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import { motion } from "framer-motion";
 import { information } from "../info";
 import { Location } from "../icons";
@@ -8,7 +7,7 @@ import { useRef, useState } from "react";
 import { User } from "../util/types";
 import useSWR from "swr";
 
-const Home: NextPage = (props: any) => {
+const Home: NextPage = () => {
     const [amountFocused, setAmountFocused] = useState(false);
     const paymentAmount = useRef<string>("1");
 
@@ -27,111 +26,96 @@ const Home: NextPage = (props: any) => {
 
     if (fetchedUserInfo && !error)
         return (
-            <>
-                <Head>
-                    <title>Sponsor @{fetchedUserInfo.login} on GitHub</title>
-                    <link rel="icon" type="image/png" href="/favicon.png" />
-                    <meta
-                        name="description"
-                        content={`Sponsor ${information.username} on an easier alternative to GitHub Sponsors.`}
-                    />
-                    <meta name="theme-color" content="#fff" />
-                    <meta name="image" content="/favicon.png" />
-                </Head>
+            <div className="w-[25rem] md:w-[36rem] h-auto flex flex-col items-center justify-center">
+                <h3 className="mt-20 text-2xl text-[#c9d1d9] font-medium mb-10 text-center">
+                    Become a Sponsor to{" "}
+                    <a className="font-semibold" href={`https://github.com/${fetchedUserInfo.login}`}>
+                        {fetchedUserInfo.name}
+                    </a>
+                </h3>
 
-                <div className="w-[25rem] md:w-[36rem] h-auto flex flex-col items-center justify-center">
-                    <h3 className="mt-20 text-2xl text-[#c9d1d9] font-medium mb-10 text-center">
-                        Become a Sponsor to{" "}
-                        <a className="font-semibold" href={`https://github.com/${fetchedUserInfo.login}`}>
+                <motion.div className="flex flex-row items-start justify-center">
+                    <img width={80} className="rounded-full" src={fetchedUserInfo.avatar_url} />
+                    <div className="flex flex-col items-start justify-center ml-5">
+                        <motion.a
+                            className="text-xl font-semibold"
+                            style={{ color: "#c9d1d9" }}
+                            whileHover={{ color: "#58a6ff", cursor: "pointer", textDecoration: "none" }}
+                            transition={{ duration: 0 }}
+                            href={`https://github.com/${information.username}`}
+                        >
                             {fetchedUserInfo.name}
-                        </a>
-                    </h3>
-
-                    <motion.div className="flex flex-row items-start justify-center">
-                        <img width={80} className="rounded-full" src={fetchedUserInfo.avatar_url} />
-                        <div className="flex flex-col items-start justify-center ml-5">
-                            <motion.a
-                                className="text-xl font-semibold"
-                                style={{ color: "#c9d1d9" }}
-                                whileHover={{ color: "#58a6ff", cursor: "pointer", textDecoration: "none" }}
-                                transition={{ duration: 0 }}
-                                href={`https://github.com/${information.username}`}
+                        </motion.a>
+                        <p className="mt-1 text-sm text-[#8b949e]">{fetchedUserInfo.login}</p>
+                        <p className="mt-1 text-sm flex flex-row">
+                            <Location fill="#8b949e" style={{ marginTop: "4px" }} />
+                            <a
+                                href={`https://www.google.com/maps/search/?api=1&query=${fetchedUserInfo.location}`}
+                                className="ml-1 text-[#58a6ff]"
                             >
-                                {fetchedUserInfo.name}
-                            </motion.a>
-                            <p className="mt-1 text-sm text-[#8b949e]">{fetchedUserInfo.login}</p>
-                            <p className="mt-1 text-sm flex flex-row">
-                                <Location fill="#8b949e" style={{ marginTop: "4px" }} />
-                                <a
-                                    href={`https://www.google.com/maps/search/?api=1&query=${fetchedUserInfo.location}`}
-                                    className="ml-1 text-[#58a6ff]"
-                                >
-                                    {fetchedUserInfo.location}
-                                </a>
-                            </p>
-                        </div>
+                                {fetchedUserInfo.location}
+                            </a>
+                        </p>
+                    </div>
+                </motion.div>
+
+                <motion.pre className="mt-6 text-[1rem] leading-[1.5] text-[#c9d1d9] w-full whitespace-pre-wrap font-default">
+                    {information.description}
+                </motion.pre>
+
+                <motion.div className="flex flex-row mt-4">
+                    <motion.div
+                        className="text-[#8b949e] border-solid border-[1px] rounded-md px-3 py-1 mr-2"
+                        style={{ borderColor: amountFocused ? "#58a6ff" : "#30363d" }}
+                        transition={{ duration: 0.05 }}
+                    >
+                        $
+                        <input
+                            type="number"
+                            min={1}
+                            placeholder="5"
+                            className="ml-1 w-[4rem] h-[2rem] bg-transparent text-white outline-none"
+                            onFocus={() => setAmountFocused(true)}
+                            onBlur={() => setAmountFocused(false)}
+                            onChange={(e: { target: HTMLInputElement }) => (paymentAmount.current = e.target.value)}
+                        />
                     </motion.div>
+                    <motion.button
+                        className="text-[#c9d1d9] px-4 py-1 text-md rounded-md"
+                        style={{ backgroundColor: "#21262d", border: "solid 1px rgba(240, 246, 252, 0.2)" }}
+                        whileHover={{ backgroundColor: "#30363d", border: "solid 1px #8b949e" }}
+                        whileTap={{ backgroundColor: "hsl(212, 12%, 18%, 1)", border: "solid 1px #6e7681" }}
+                        transition={{ duration: 0.15 }}
+                        onClick={() =>
+                            window.open(
+                                information.amount_slug
+                                    ? `${information.payment_url}/${paymentAmount.current}`
+                                    : information.payment_url,
+                                "_self"
+                            )
+                        }
+                    >
+                        Donate
+                    </motion.button>
+                </motion.div>
 
-                    <motion.pre className="mt-6 text-[1rem] leading-[1.5] text-[#c9d1d9] w-full whitespace-pre-wrap font-default">
-                        {information.description}
-                    </motion.pre>
+                <motion.div className="mt-10 mb-16 w-full">
+                    <h3 className="text-xl text-[#8b949e] font-thin mb-4 text-center md:text-left">Featured Work</h3>
+                    <div className="w-full h-auto grid grid-flow-row grid-cols-1 md:grid-cols-2 gap-4 auto-rows-fr">
+                        {information.featuredRepos.map((repoName: string) => (
+                            <RepoComponent key={repoName} name={repoName} />
+                        ))}
+                    </div>
+                </motion.div>
 
-                    <motion.div className="flex flex-row mt-4">
-                        <motion.div
-                            className="text-[#8b949e] border-solid border-[1px] rounded-md px-3 py-1 mr-2"
-                            style={{ borderColor: amountFocused ? "#58a6ff" : "#30363d" }}
-                            transition={{ duration: 0.05 }}
-                        >
-                            $
-                            <input
-                                type="number"
-                                min={1}
-                                placeholder="5"
-                                className="ml-1 w-[4rem] h-[2rem] bg-transparent text-white outline-none"
-                                onFocus={() => setAmountFocused(true)}
-                                onBlur={() => setAmountFocused(false)}
-                                onChange={(e: { target: HTMLInputElement }) => (paymentAmount.current = e.target.value)}
-                            />
-                        </motion.div>
-                        <motion.button
-                            className="text-[#c9d1d9] px-4 py-1 text-md rounded-md"
-                            style={{ backgroundColor: "#21262d", border: "solid 1px rgba(240, 246, 252, 0.2)" }}
-                            whileHover={{ backgroundColor: "#30363d", border: "solid 1px #8b949e" }}
-                            whileTap={{ backgroundColor: "hsl(212, 12%, 18%, 1)", border: "solid 1px #6e7681" }}
-                            transition={{ duration: 0.15 }}
-                            onClick={() =>
-                                window.open(
-                                    information.amount_slug
-                                        ? `${information.payment_url}/${paymentAmount.current}`
-                                        : information.payment_url,
-                                    "_self"
-                                )
-                            }
-                        >
-                            Donate
-                        </motion.button>
-                    </motion.div>
-
-                    <motion.div className="mt-10 mb-16 w-full">
-                        <h3 className="text-xl text-[#8b949e] font-thin mb-4 text-center md:text-left">
-                            Featured Work
-                        </h3>
-                        <div className="w-full h-auto grid grid-flow-row grid-cols-1 md:grid-cols-2 gap-4 auto-rows-fr">
-                            {information.featuredRepos.map((repoName: string) => (
-                                <RepoComponent key={repoName} name={repoName} />
-                            ))}
-                        </div>
-                    </motion.div>
-
-                    <p className="text-[#8b949e] text-sm mb-12">
-                        Psst... this project is{" "}
-                        <a className="no-underline text-[#e35454]" href="https://github.com/cnrad/sponsor">
-                            open source
-                        </a>
-                        !
-                    </p>
-                </div>
-            </>
+                <p className="text-[#8b949e] text-sm mb-12">
+                    Psst... this project is{" "}
+                    <a className="no-underline text-[#e35454]" href="https://github.com/cnrad/sponsor">
+                        open source
+                    </a>
+                    !
+                </p>
+            </div>
         );
 
     return (
